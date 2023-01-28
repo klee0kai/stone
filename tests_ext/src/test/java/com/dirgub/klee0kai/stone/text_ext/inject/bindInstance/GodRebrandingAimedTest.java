57e7ffc494dd634a21778f869ae4f5e78c154a87
klee0kai <klee0kai@gmail.com>
2023-01-28 23:34:31 +0400

    bind aimed tests

---
+++ tests_ext/src/test/java/com/dirgub/klee0kai/stone/text_ext/inject/bindInstance/GodRebrandingAimedTest.java
@@ -0,0 +1,102 @@
+package com.dirgub.klee0kai.stone.text_ext.inject.bindInstance;
+
+import com.github.klee0kai.stone.Stone;
+import com.github.klee0kai.test.di.bindinstance.simple.GodWorkspaceComponent;
+import com.github.klee0kai.test.mowgli.galaxy.Earth;
+import com.github.klee0kai.test_ext.inject.di.bindinstance.GodRebrandingComponent;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class GodRebrandingAimedTest {
+
+    @Test
+    public void createdIsReusableTest() {
+        // Given
+        GodWorkspaceComponent DI = Stone.createComponent(GodWorkspaceComponent.class);
+        Earth earth = new Earth();
+        DI.bindEarth(earth);
+
+
+        //When
+        GodRebrandingComponent DIPro = Stone.createComponent(GodRebrandingComponent.class);
+        DIPro.extOf(DI);
+
+
+        //Then
+        assertEquals(earth, DI.sunSystem().earth());
+        assertEquals(earth, DIPro.sunSystem().earth());
+        assertNull(DI.sunSystem().planet());
+        assertNull(DIPro.sunSystem().planet());
+    }
+
+    @Test
+    public void createdAfterRebrandingTest() {
+        // Given
+        GodWorkspaceComponent DI = Stone.createComponent(GodWorkspaceComponent.class);
+        GodRebrandingComponent DIPro = Stone.createComponent(GodRebrandingComponent.class);
+        DIPro.extOf(DI);
+        Earth earth = new Earth();
+
+
+        //When
+        DI.bindEarth(earth);
+
+
+        //Then
+        assertEquals(earth, DI.sunSystem().earth());
+        assertEquals(earth, DIPro.sunSystem().earth());
+        assertNull(DI.sunSystem().planet());
+        assertNull(DIPro.sunSystem().planet());
+    }
+
+
+    @Test
+    public void rebrandedSunTest() {
+        // Given
+        GodWorkspaceComponent DI = Stone.createComponent(GodWorkspaceComponent.class);
+        Earth earth1 = new Earth();
+        Earth earth2 = new Earth();
+        DI.bindEarth(earth1);
+
+
+        //When
+        GodRebrandingComponent DIPro = Stone.createComponent(GodRebrandingComponent.class);
+        DIPro.extOf(DI);
+        DIPro.bindEarth(earth2);
+
+
+        //Then
+        assertEquals(earth2, DI.sunSystem().earth());
+        assertEquals(earth2, DIPro.sunSystem().earth());
+        assertNull(DI.sunSystem().planet());
+        assertNull(DIPro.sunSystem().planet());
+    }
+
+
+    @Test
+    public void updatedAfterRebrandingSunTest() {
+        // Given
+        GodWorkspaceComponent DI = Stone.createComponent(GodWorkspaceComponent.class);
+        Earth earth1 = new Earth();
+        Earth earth2 = new Earth();
+        Earth earth3 = new Earth();
+        DI.bindEarth(earth1);
+        GodRebrandingComponent DIPro = Stone.createComponent(GodRebrandingComponent.class);
+        DIPro.extOf(DI);
+        DIPro.bindEarth(earth2);
+
+
+        //When
+        DI.bindEarth(earth3);
+
+
+        //Then
+        assertEquals(earth3, DI.sunSystem().earth());
+        assertEquals(earth3, DIPro.sunSystem().earth());
+        assertNull(DI.sunSystem().planet());
+        assertNull(DIPro.sunSystem().planet());
+    }
+
+}
