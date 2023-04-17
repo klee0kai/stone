7fb8559c11b3bd3c7d26292aedd648d5ec3b6ede
klee0kai <klee0kai@gmail.com>
2023-04-17 21:24:30 +0400

    cache is implemented interface

---
+++ stone_lib/src/main/java/com/github/klee0kai/stone/closed/IPrivateCachedImplemented.java
@@ -0,0 +1,9 @@
+package com.github.klee0kai.stone.closed;
+
+/**
+ * Stone Private class
+ * <p>
+ * This class is used to mark classes that already implemented provide cached (by component or module)
+ */
+public class IPrivateCachedImplemented {
+}
