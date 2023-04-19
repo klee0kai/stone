0cb0f41f01ae44be1d77a242ecbaa151a4657703
klee0kai <klee0kai@gmail.com>
2023-04-19 22:02:50 +0400

    review remarks, remove useless entry points

--- test_feature/finance/api/src/main/java/com/github/klee0kai/stone/test_feature/finance/model/BalanceInfo.java
+++ test_feature/finance/api/src/main/java/com/github/klee0kai/stone/test_feature/finance/model/BalanceInfo.java
@@ -1,6 +1,7 @@
 package com.github.klee0kai.stone.test_feature.finance.model;
 
 public class BalanceInfo {
+
     public Long balance;
 
 }
--- test_feature/finance/impl/src/main/java/com/github/klee0kai/stone/test_feature/finance/Main.java
+++
@@ -1,7 +0,0 @@
-package com.github.klee0kai.stone.test_feature.finance;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
