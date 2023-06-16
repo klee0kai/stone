3c79d205da537617435efc5be89853cff85f69c9
klee0kai <klee0kai@gmail.com>
2023-06-16 22:47:04 +0400

    remove component's interface support - AccountingComponent

--- test_feature/finance/impl/src/main/java/com/github/klee0kai/stone/test_feature/finance/di/AccountingComponent.java
+++ test_feature/finance/impl/src/main/java/com/github/klee0kai/stone/test_feature/finance/di/AccountingComponent.java
@@ -2,10 +2,9 @@ package com.github.klee0kai.stone.test_feature.finance.di;
 
 import com.github.klee0kai.stone.annotations.component.Component;
 import com.github.klee0kai.stone.annotations.component.Init;
-import com.github.klee0kai.stone.closed.IComponent;
 
 @Component
-public interface AccountingComponent extends IComponent {
+public interface AccountingComponent {
 
     AccountingDependencies dependencies();
 
