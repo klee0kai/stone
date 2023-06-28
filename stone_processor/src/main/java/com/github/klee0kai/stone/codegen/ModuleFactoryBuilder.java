a50b7fe4a55bf6c32816413da1cf89937824dec0
klee0kai <klee0kai@gmail.com>
2023-06-28 22:35:31 +0400

    support provide with wrappers in module

--- stone_processor/src/main/java/com/github/klee0kai/stone/codegen/ModuleFactoryBuilder.java
+++ stone_processor/src/main/java/com/github/klee0kai/stone/codegen/ModuleFactoryBuilder.java
@@ -3,14 +3,16 @@ package com.github.klee0kai.stone.codegen;
 import com.github.klee0kai.stone.closed.IModuleFactory;
 import com.github.klee0kai.stone.closed.types.ListUtils;
 import com.github.klee0kai.stone.exceptions.ObjectNotProvidedException;
+import com.github.klee0kai.stone.helpers.codebuilder.SmartCode;
+import com.github.klee0kai.stone.helpers.wrap.WrapHelper;
 import com.github.klee0kai.stone.model.ClassDetail;
 import com.github.klee0kai.stone.model.FieldDetail;
 import com.github.klee0kai.stone.model.MethodDetail;
 import com.github.klee0kai.stone.model.annotations.BindInstanceAnn;
 import com.github.klee0kai.stone.utils.CodeFileUtil;
 import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 
 import javax.lang.model.element.Modifier;
@@ -21,6 +23,7 @@ import java.util.Set;
 
 import static com.github.klee0kai.stone.AnnotationProcessor.allClassesHelper;
 import static com.github.klee0kai.stone.exceptions.ExceptionStringBuilder.createErrorMes;
+import static com.github.klee0kai.stone.helpers.wrap.WrapHelper.nonWrappedType;
 import static com.github.klee0kai.stone.utils.StoneNamingUtils.genFactoryNameMirror;
 import static com.squareup.javapoet.MethodSpec.methodBuilder;
 
@@ -44,20 +47,10 @@ public class ModuleFactoryBuilder {
             for (MethodDetail m : module.getAllMethods(false, false, "<init>")) {
                 if (!m.isAbstract() && !module.isInterfaceClass())
                     continue;
-                ClassDetail providingClass = allClassesHelper.findForType(m.returnType);
-                boolean hasConstructor = providingClass.findMethod(MethodDetail.constructorMethod(m.args), false) != null;
                 if (m.hasAnnotations(BindInstanceAnn.class)) {
-                    builder.provideNullMethod(m.methodName, m.returnType, m.args);
-                } else if (!hasConstructor) {
-                    List<String> argTypes = ListUtils.format(m.args, (it) -> it.type.toString());
-                    throw new ObjectNotProvidedException(
-                            createErrorMes()
-                                    .constructorNonFound(providingClass.className.toString(), argTypes)
-                                    .build(),
-                            null
-                    );
+                    builder.provideNullMethod(m);
                 } else {
-                    builder.provideMethod(m.methodName, m.returnType, m.args);
+                    builder.provideMethod(m);
                 }
             }
         }
@@ -70,34 +63,52 @@ public class ModuleFactoryBuilder {
     }
 
 
-    public ModuleFactoryBuilder provideMethod(String name, TypeName provideCl, List<FieldDetail> args) {
-        MethodSpec.Builder builder = methodBuilder(name)
+    public ModuleFactoryBuilder provideMethod(MethodDetail m) {
+        ClassDetail providingClass = allClassesHelper.findForType(nonWrappedType(m.returnType));
+        boolean hasConstructor = providingClass.findMethod(MethodDetail.constructorMethod(m.args), false) != null;
+        if (!hasConstructor) {
+            List<String> argTypes = ListUtils.format(m.args, (it) -> it.type.toString());
+            throw new ObjectNotProvidedException(
+                    createErrorMes()
+                            .constructorNonFound(providingClass.className.toString(), argTypes)
+                            .build(),
+                    null
+            );
+        }
+
+        MethodSpec.Builder builder = methodBuilder(m.methodName)
                 .addModifiers(Modifier.PUBLIC)
                 .addAnnotation(Override.class)
-                .returns(provideCl);
+                .returns(m.returnType);
+        for (FieldDetail p : m.args)
+            builder.addParameter(p.type, p.name);
 
-        if (args == null || args.isEmpty()) {
-            builder.addStatement("return new $T()", provideCl);
-        } else {
-            for (FieldDetail p : args)
-                builder.addParameter(p.type, p.name);
-            builder.addStatement("return new $T($L)", provideCl,
-                    String.join(",", ListUtils.format(args, (it) -> it.name)));
-        }
+        String argStr = m.args == null ? "" : String.join(",", ListUtils.format(m.args, (it) -> it.name));
+        SmartCode genCode = SmartCode.builder()
+                .add(CodeBlock.of("new $T( $L )", providingClass.className, argStr))
+                .providingType(providingClass.className);
+
+        builder.addCode(
+                SmartCode.builder()
+                        .add("return ")
+                        .add(WrapHelper.transform(genCode, m.returnType))
+                        .add(";\n")
+                        .build()
+        );
 
 
         provideMethodBuilders.add(builder);
         return this;
     }
 
-    public ModuleFactoryBuilder provideNullMethod(String name, TypeName provideCl, List<FieldDetail> args) {
-        MethodSpec.Builder builder = methodBuilder(name)
+    public ModuleFactoryBuilder provideNullMethod(MethodDetail m) {
+        MethodSpec.Builder builder = methodBuilder(m.methodName)
                 .addModifiers(Modifier.PUBLIC)
                 .addAnnotation(Override.class)
-                .returns(provideCl)
+                .returns(m.returnType)
                 .addStatement("return null");
 
-        if (args != null) for (FieldDetail p : args)
+        if (m.args != null) for (FieldDetail p : m.args)
             builder.addParameter(p.type, p.name);
 
         provideMethodBuilders.add(builder);
--- tests_deps/src/main/java/com/github/klee0kai/test/car/di/CarFactory.java
+++ tests_deps/src/main/java/com/github/klee0kai/test/car/di/CarFactory.java
@@ -1,16 +1,29 @@
 package com.github.klee0kai.test.car.di;
 
 import com.github.klee0kai.stone.annotations.module.Module;
+import com.github.klee0kai.stone.types.wrappers.LazyProvide;
+import com.github.klee0kai.stone.types.wrappers.Ref;
 import com.github.klee0kai.test.car.Bumper;
 import com.github.klee0kai.test.car.Car;
 import com.github.klee0kai.test.car.Whell;
+
+import javax.inject.Provider;
+import java.lang.ref.WeakReference;
 
 @Module
 public interface CarFactory {
 
     Car car(Bumper bumper, Whell whell);
 
+    LazyProvide<WeakReference<Car>> carRef(Bumper bumper, Whell whell);
+
+
     Whell whell();
+
+    Provider<Whell> whellProvider();
+
+
+    WeakReference<Ref<Ref<Whell>>> whellRef();
 
 
     Bumper bumper();
